b022b49c3ecfcf0dbd5edbef3ec9941d63666ea4
Rafael Souza <rafael.gustavo221188@gmail.com>
2022-07-12 23:19:00 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="rsouza-DSMeta" />
+                <h1>rsouza-DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/rafael-souza22/"> Rafael Souza</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
